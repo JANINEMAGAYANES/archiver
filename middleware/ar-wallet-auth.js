7e64c95fbedb03ef1ddc4bf866ff1bf4abaf0dd5
aidanok <52333119+aidanok@users.noreply.github.com>
2020-01-28 09:29:58 +0000

    minor comments and adjustments

--- middleware/ar-wallet-auth.js
+++ middleware/ar-wallet-auth.js
@@ -12,19 +12,19 @@ async function verifyArWalletSignature({ public_key, signature }) {
   
   var arwallet = await arweave.wallets.ownerToAddress(public_key);
 
-  // Decode the wallet into bytes ()
-
-  var arwalletAsBytes = arweave.utils.b64UrlToBuffer(arwallet);
+  // Convert the wallet string to bytes, so its the same thing the 
+  // user signed and we can verify if.
+  var arwalletAsBytes = arweave.utils.stringToBuffer(arwallet);
   
-  // Decode the signature into bytes. 
+  // Decode the signature from base64url into bytes. 
   var signatureAsBytes = arweave.utils.b64UrlToBuffer(signature);
  
-  var verified = await arweave.crypto.verify(public_key, arwalletAsBytes, signatureAsBytes);
+  var result = await arweave.crypto.verify(public_key, arwalletAsBytes, signatureAsBytes);
 
-  if (verified) {
-    return { verified, arwallet }
+  if (result) {
+    return { verified: result, arwallet: arwallet }
   } else {
-    return { verified }
+    return { verified: result }
   }
 }
 
@@ -33,15 +33,23 @@ async function verifyArWalletSignature({ public_key, signature }) {
 // This function should be run client side, and sent up to the server.
 // It will return an object like { public_key, signature } 
 // that can be passed to verifyArWalletSignature 
+
+// what we sign doesn't really matter actually, we use the public wallet address
+// but it could just be any string like '12345', we need to convert it to bytes 
+// to sign it.
+
 async function generateArWalletSignature(wallet) {
+
   var arwallet = await arweave.wallets.jwkToAddress(wallet);
 
-  // Decode the wallet into bytes ()
+  // Convert the wallet address string into bytes so we can sign it. 
 
-  var arwalletAsBytes = arweave.utils.b64UrlToBuffer(arwallet);
+  var arwalletAsBytes = arweave.utils.stringToBuffer(arwallet);
   
+  // Signature is always returned as bytes, we encode it a base64url string 
+  // afterwards so we can transfer to server as JSON easily. 
   var signatureAsBytes = await arweave.crypto.sign(wallet, arwalletAsBytes);
-
+  
   var signature = arweave.utils.bufferTob64Url(signatureAsBytes);
 
   return { public_key: wallet.n, signature };
